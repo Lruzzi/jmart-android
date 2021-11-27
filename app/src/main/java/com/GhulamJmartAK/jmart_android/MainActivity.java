84fe60d1a2fcc9f16308fed3094f80d0a9ac95ee
Ghulam Izzul Fuad <ghulam.izzul@ui.ac.id>
2021-11-27 15:00:50 +0700

    CS Modul 9 OOP

---
+++ app/src/main/java/com/GhulamJmartAK/jmart_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.GhulamJmartAK.jmart_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView userMain = findViewById(R.id.textMain);
+        userMain.setText("Welcome " + LoginActivity.getLoggedAccount().name);
+    }
+}
---
+++ app/src/main/java/com/GhulamJmartAK/jmart_android/model/Account.java
@@ -0,0 +1,9 @@
+package com.GhulamJmartAK.jmart_android.model;
+
+public class Account extends Serializable{
+    public double balance;
+    public String email;
+    public String name;
+    public String password;
+    public Store store;
+}
